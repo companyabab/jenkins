082ae3e8603f27aa315b4d692d51721e56f1e99e
companyabab <30520002+companyabab@users.noreply.github.com>
2017-09-04 17:02:59 -0400

    Update date_time.py

--- date_time.py
+++ date_time.py
@@ -3,6 +3,6 @@ now = datetime.datetime.now()
 print ("Current date and time : ")
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
 print ("hello world")
-print ("hello manohar")
-print ("hello manohar")
 print ("hello sumanth Donraju")
+print ("class is at 5pm pst ")
+
