a3f5dd6b2a6c65cfdb98f677c0fa5ec0b553bb86
companyabab <30520002+companyabab@users.noreply.github.com>
2017-09-04 19:09:02 -0400

    Update date_time.py

--- date_time.py
+++ date_time.py
@@ -4,7 +4,7 @@ print ("Current date and time : ")
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
 print ("hello world")
 print ("hello sumanth Donraju")
-print ("class is at 5pm pst ")
-print ("hello")
+print ("added new trigger")
+print ("check ")
 
 
